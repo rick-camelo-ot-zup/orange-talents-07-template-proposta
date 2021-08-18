f85a12593a7ad4396cefebedd181d21edf9e1834
Rick Camelo Martins <rick.martins@zup.com.br>
2021-08-18 16:14:20 -0300

    Adicionado endpoint prometheus.

--- src/main/java/br/rickcm/proposta/config/SecurityConfiguration.java
+++ src/main/java/br/rickcm/proposta/config/SecurityConfiguration.java
@@ -1,27 +1,48 @@
 package br.rickcm.proposta.config;
 
-import org.springframework.http.HttpMethod;
+import org.keycloak.adapters.KeycloakConfigResolver;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
-@EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+@EnableGlobalMethodSecurity(jsr250Enabled = true)
+public class SecurityConfiguration extends KeycloakWebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests(authorizeRequests ->
-                authorizeRequests
-                        .antMatchers(HttpMethod.GET, "/propostas/**").hasAuthority("SCOPE_proposta-user:read")
-                        .antMatchers(HttpMethod.GET, "/cartoes/**").hasAuthority("SCOPE_proposta-user:read")
-                        .antMatchers(HttpMethod.GET, "/biometrias/**").hasAuthority("SCOPE_proposta-user:read")
-                        .antMatchers(HttpMethod.POST, "/cartoes/**").hasAuthority("SCOPE_proposta-admin")
-                        .antMatchers(HttpMethod.POST, "/propostas/**").hasAuthority("SCOPE_proposta-user:write")
-                        .antMatchers(HttpMethod.POST, "/biometrias/**").hasAuthority("SCOPE_proposta-user:write")
-                        .anyRequest().authenticated()
-        )
-                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+        super.configure(http);
+        http.authorizeRequests()
+                .anyRequest().permitAll()
+//                .anyRequest()
+//                .authenticated()
+                .and()
+                .csrf().disable();
+        http.csrf().disable();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
+        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+        auth.authenticationProvider(keycloakAuthenticationProvider);
+    }
+    @Bean
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+    @Bean
+    public KeycloakConfigResolver KeycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
     }
 
 }
